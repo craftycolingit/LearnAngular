3a2aae77f888ae864d2b4241adad5f31435b9f69
craftycolingit <hello@colinjaggard.co.uk>
2024-07-03 23:05:00 +0100

    Add elastic package and set up dev env
    Added in the elasticsearch package.
    Set up the dev environment to also run in Docker separate to the prod env.

--- ticketmaster/src/environments/environment.ts
+++ ticketmaster/src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
     production: false,
-    API_BASE_URL: 'http://localhost:3000',
+    API_BASE_URL: 'http://192.168.0.41:3000',
+    ELASTIC_PASSWORD: 'superSecretPassword',
+    KIBANA_PASSWORD: 'superSecretPassword',
 }
